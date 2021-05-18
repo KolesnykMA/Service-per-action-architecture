b141cad72bb27e5bf417e18702d3ebdb8c166023
TobiMaxKenobi <maks.k@intobi.com>
2021-05-18 10:05:25 +0300

    Init

---
+++ server.js
@@ -0,0 +1,17 @@
+require('dotenv').config();
+
+const mobileApi = require('./lib/api/app');
+// const domain = require('./lib/domain');
+
+const appConfig = require('./lib/config/appConfig');
+// const dbConfig = require('./lib/config/dbConfig');
+
+async function server() {
+  mobileApi.start({appPort: appConfig.port});
+  // await domain.initializeDomain(dbConfig);
+}
+
+server().catch((error) => {
+  console.error(error);
+  process.exit(1);
+});
